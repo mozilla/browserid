3b6f5a7362b5f065e304915874bade6a36b7e6c6
David Caro <david.caro.estevez@gmail.com>
2012-07-31 14:30:42 +0200

    Fixes: mozilla/browserid#1859 Using the new version string format (and some minor improvements)

--- scripts/rpmbuild.sh
+++ scripts/rpmbuild.sh
@@ -3,14 +3,90 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-set -e
+usage()
+{
+    cat <<EOU
+    Usage: $0 [-hv] [major]
+
+    Creates an rpm package of the current version
+
+    Options:
+        -h
+            Show this help
+        -v
+            Be verbose
+
+    Parameters:
+        major
+            Major version to use, defaults to '0'
+
+
+EOU
+    exit ${1:-0}
+}
+
+get_locale()
+{
+    [[ -d locale/.svn ]] \
+    && svn up locale \
+    || {
+        [[ -d locale ]] \
+        || svn co  http://svn.mozilla.org/projects/l10n-misc/trunk/browserid/locale
+    }   
+    LOCALE_REV="$(svn info locale/ | sed -n -e "s,^Rev.*n: ,,p")"
+}
+
+get_ver_string()
+{
+    branch="$(git branch | grep '*')"
+    branch="${branch:2}"
+    describe_all="$(git describe --all --long)"
+    describe_tags="$(git describe --tags --long)"
+    [[ $? -ne 0 ]] \
+    && {
+        ## we have no reachable tags in the history, use the branch name
+        echo "${describe_all#*/}"
+        return
+    }   
+    [[ "${describe_tags%-*-*}" == "${describe_all#*/}" ]] \
+    && {
+        ## the tag we got with the describe is newer than the branch, use it
+        echo "$describe_tags"
+        return
+    } || {
+        ## Iep, we got a tag that is older than the current branch, use the
+        ## branch name
+        echo "${describe_all#*/}"
+        return
+    }   
+}
+
+get_ci_count()
+{
+    git log --oneline ${1:+$1..} --pretty='format:%h' | wc -l
+}
+
+
+#### MAIN
+
+while getopts 'hv' option; do
+    case $option in
+        h) usage;;
+        v) set -e;;
+        *) usage 1;;
+    esac
+done
+shift $((OPTIND - 1))
+
+MAJOR=${1:-0}
+
 
 progname=$(basename $0)
 
 cd $(dirname $0)/..    # top level of the checkout
 
-mkdir -p rpmbuild/SOURCES rpmbuild/SPECS rpmbuild/SOURCES
-rm -rf rpmbuild/RPMS rpmbuild/SOURCES/browserid
+rm -rf rpmbuild/RPMS rpmbuild/SOURCES/browserid &>/dev/null
+mkdir -p rpmbuild/{SOURCES,SPECS,RPMS}
 
 tar --exclude rpmbuild --exclude .git \
     --exclude var -czf \
@@ -18,11 +94,18 @@ tar --exclude rpmbuild --exclude .git \
 
 set +e
 
-export GIT_REVISION=$(git log -1 --oneline)
-export SVN_REVISION=$(svn info locale/ | sed -n -e "s,^Revision: ,,p")
+## this also sets the LOCALE_REV variable
+get_locale
 
+ver_string="$(get_ver_string)"
+ver="${ver_string%-*-*}"
+rel="${ver_string:$((${#ver} + 1))}"
+## the char '-' is not allowed in the version nor the release
 rpmbuild --define "_topdir $PWD/rpmbuild" \
-         --define "svnrev $SVN_REVISION" -ba scripts/browserid.spec
+         --define "ver $MAJOR.${ver//-/_}" \
+         --define "rel ${rel//-/.}" \
+         --define "rev_locale $LOCALE_REV" \
+         -ba scripts/browserid.spec
 rc=$?
 if [ $rc -eq 0 ]; then
     ls -l $PWD/rpmbuild/RPMS/*/*.rpm
